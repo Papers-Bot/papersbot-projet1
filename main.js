b85f1a33e5866cf2b11177458991085e6723ba3b
Papers-RSH <kelola@dot-coin.com>
2019-09-10 19:31:16 +0200

    Modification gitignore + Fichier en moins

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const { Client } = require("discord.js");
-const client = new Client();
+const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
   console.log(`Connecté en tant que ${client.user.tag}`);
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong !");
-  }
+  if (msg.content === "ping") msg.channel.send("Pong !");
+  if (msg.content === "everyone")
+    msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
+  if (msg.content === "noteveryone")
+    msg.channel.send("@everyone, salut à tous !");
 });
 
-client.login(process.env.CLIENT_TOKEN);
+client.login("NjIxMDI2MTU0NjY4NzUyOTA2.XXfdAQ.Wjr_13x3wCBCLFwGlFOD_P-Oqx0");
