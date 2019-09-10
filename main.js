96d0ec1cd0fa9045f6646527ef47278ce95a2b48
Papers-RSH <kelola@dot-coin.com>
2019-09-10 19:47:33 +0200

    Changement Du Bot

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config.js");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Connecté en tant que ${client.user.tag}`);
+  console.log("Je suis prêt !");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("Pong !");
-  if (msg.content === "everyone")
-    msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone, salut à tous !");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong !");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping !");
 });
 
-client.login("NjIxMDI2MTU0NjY4NzUyOTA2.XXfdAQ.Wjr_13x3wCBCLFwGlFOD_P-Oqx0");
+client.login(TOKEN);
